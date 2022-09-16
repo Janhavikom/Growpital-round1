d39b247f96bb202ddc22ef476ac1d7cae5822685
Janhavikom <84494006+Janhavikom@users.noreply.github.com>
2022-09-16 22:00:45 +0530

    Add files via upload

---
+++ Task1/task1.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "205px";
+    document.getElementById("menu-font").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("menu-font").style.display = "";
+  }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
